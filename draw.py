9365e33b8618b2e8e155a8d24dbf4d2cbab13088
huiminthehuman <huiminwu24@gmail.com>
2019-02-08 15:34:45 -0500

    Make sure code can detect octants to speed up coding process in the future

--- draw.py
+++ draw.py
@@ -8,32 +8,71 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     delX = x1 - x0 + 0.0
     delY = y1 - y0 + 0.0
     if (delX == 0):
-        plot(screen, color, x, y)
-        while y <= y1:
-            y = y + 1
+        print("line is vertical")
+        #plot(screen, color, x, y)
+        #while y < y1:
+        #    y = y + 1
     elif (delY == 0):
-        plot(screen, color, x, y)
-        while x <= x1:
-            x = x + 1
+        print("line is horizantal")
+        #plot(screen, color, x, y)
+        #while x < x1:
+        #    x = x + 1
     else:
         slope = delY / delX
-        if (slope < 1 and slope > 0) :
-            # OCTANT 1
-            d = 2 * A + B
-            while x <= x1:
-                plot(screen, color, x,y)
-                if d > 0:
-                    y = y + 1
-                    d = d + 2 * B
-                x = x + 1
-                d = d + 2 * A
-        elif (slope > 1) :
-            # OCTANT 2
-            d = A + 2 * B
-            while y <= y1:
-                plot(screen, color, x, y)
-                if d < 0:
-                    x = x + 1
-                    d = d + 2 * A
-                y = y + 1
-                d = d + 2 * B
+        if (delY > 0): #if in quadrant 1 and 2
+            if (delX > 0):
+                #quadrant 1
+                if (slope < 1):
+                    #octant 1
+                    print("octant 1 line")
+                else:
+                    #octant 2
+                    print("octant 2 line")
+            else:
+                #quadrant 2
+                if (slope < -1):
+                    #octant 3
+                    print("octant 3 line")
+                else:
+                    #octant 4
+                    print("octant 4 line")
+
+        else:  #if in quadrant 3 and 4
+            if (delX < 0):
+                #quadrant 3
+                if (slope < 1) :
+                    #octant 5
+                    print("octant 5 line")
+                else:
+                    #octant 6
+                    print("octant 6 line")
+            else:
+                #quadrant 4
+                if (slope < -1 ) :
+                    #octant 7
+                    print("octant 7 line")
+                else:
+                    #octant 8
+                    print("octant 8 line")
+
+        #if (slope < 1 and slope > 0) :
+        #    # OCTANT 1
+        #    d = 2 * A + B
+        #    while x <= x1:
+        #        plot(screen, color, x,y)
+        #        if d > 0:
+        #            y = y + 1
+        #            d = d + 2 * B
+        #        x = x + 1
+        #        d = d + 2 * A
+        #elif (slope > 1) :
+        #    # OCTANT 2
+        #    d = A + 2 * B
+        #    while y <= y1:
+        #        plot(screen, color, x, y)
+        #        if d < 0:
+        #            x = x + 1
+        #            d = d + 2 * A
+        #        y = y + 1
+        #        d = d + 2 * B
+        #elif (slope 
--- main.py
+++ main.py
@@ -5,13 +5,25 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 
 #octant 1
-draw_line(250, 250, 500, 350, screen, color)
+draw_line(250, 250, 400, 300, screen, color)
 #octant 2
-draw_line(250, 250, 350, 500, screen, color)
+draw_line(250, 250, 300, 400, screen, color)
+#octant 3
+draw_line(250, 250, 200, 400, screen, color)
+#octant 4
+draw_line(250, 250, 100, 300, screen, color)
+#octant 5
+draw_line(250, 250, 100, 200, screen, color)
+#octant 6
+draw_line(250, 250, 200, 100, screen, color)
+#octant 7
+draw_line(250, 250, 300, 100, screen, color)
+#octant 8
+draw_line(250, 250, 400, 200, screen, color)
 #slope = 0
-draw_line(250, 250, 250, 250, screen, color)
+draw_line(250, 250, 500, 250, screen, color)
 #slope = 1
-draw_line(250, 250, 500, 500, screen, color)
+draw_line(250, 250, 250, 500, screen, color)
 
 #for i in range(200) :
 #    plot(screen, color, i, i)
